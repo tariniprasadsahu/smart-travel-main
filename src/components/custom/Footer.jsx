933b56e2472ceca8a1edf787afe6630b6cf4aea4
tariniprasadsahu <tariniprasadsahu2002@gmail.com>
2024-11-20 20:49:57 +0530

    New Changes

--- src/components/custom/Footer.jsx
+++ src/components/custom/Footer.jsx
@@ -6,10 +6,10 @@ function Footer() {
       {/* <hr /> */}
       <hr />
       <br />
-      <h2 className='text-center text-gray-400'> &lt; Built by : Ritik &gt; </h2>
+      <h2 className='text-center text-gray-400'> &lt; Built by : Tarini &gt; </h2>
       <h2 className='text-center text-gray-400'> Support the development by Starring it ⭐</h2>
       <div className="flex justify-center py-2">
-        <a  href="https://github.com/ritikkumar-agrawal/smart-travel">
+        <a  href="https://github.com/tariniprasadsahu/smart-travel-main">
         <button className="bg-black border border-white rounded-none text-white px-3 py-1">Star it</button>
         </a>
       </div>
